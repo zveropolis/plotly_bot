4493afbd49ac101afb6c98d1ad0ecdc0d3533af2
zveropolis <zadira-era@yandex.ru>
2024-09-26 17:21:29 +0300

    send _serv.py

--- _serv.py
+++ _serv.py
@@ -1,15 +1,18 @@
 #!./.venv/bin/python
 
 
-import asyncio
+from datetime import datetime
 import logging
 import logging.config
 import os
+from pprint import pprint
 import sys
-from fastapi import FastAPI
+from typing import Annotated, List
+from fastapi import FastAPI, File, Form, Query, UploadFile, Request
+from pydantic import BaseModel
 import uvicorn
 
-
+sys.path.insert(1, os.path.join(sys.path[0], "server"))
 sys.path.insert(1, os.path.join(sys.path[0], "src"))
 
 
@@ -19,10 +22,49 @@ logger = logging.getLogger()
 app = FastAPI()
 
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
 @app.get("/test")
 def get_all_students_course(name: str | None = None):
+    pass
     return {"message": f"Hello {name}"}
 
 
+@app.post("/bot/notice")
+def receive_data(
+    notification_type: str = Form(...),
+    operation_id: str = Form(...),
+    amount: float = Form(...),
+    withdraw_amount: float = Form(...),
+    currency: str = Form(...),
+    datetime: datetime = Form(...),
+    sender: str = Form(...),
+    codepro: bool = Form(...),
+    label: str = Form(...),
+    sha1_hash: str = Form(...),
+    test_notification: bool = Form(...),
+    unaccepted: bool = Form(...),
+):
+    data = {
+        "notification_type": notification_type,
+        "operation_id": operation_id,
+        "amount": amount,
+        "withdraw_amount": withdraw_amount,
+        "currency": currency,
+        "datetime": datetime,
+        "sender": sender,
+        "codepro": codepro,
+        "label": label,
+        "sha1_hash": sha1_hash,
+        "test_notification": test_notification,
+        "unaccepted": unaccepted,
+    }
+    pprint(data)
+
+
 if __name__ == "__main__":
-    uvicorn.run(app,host="172.17.0.1", port=5000)
+    uvicorn.run(app, host="172.17.0.1", port=5000)
+    # uvicorn.run("_serv:app", host="127.0.0.1", port=5000, workers=2, reload=True)
